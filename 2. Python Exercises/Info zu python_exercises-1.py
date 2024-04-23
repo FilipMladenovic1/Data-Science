d5e900dd21179232806ff48cbf78df432204fc73
FilipMladenovic1 <120268533+FilipMladenovic1@users.noreply.github.com>
2024-04-23 15:22:52 +0200

    Create Info zu python_exercises-1.py

---
+++ 2. Python Exercises/Info zu python_exercises-1.py
@@ -0,0 +1,2 @@
+If you run the code of the first exercise (python_exercises-1.py), you have to scroll all the way up and all the way down once inside the console output,
+in order to read the entire output, since the function without NumPy for List3 prints all 1000000 integers, which is intentional.
